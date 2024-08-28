11e880d6efe4d9e26b99ff090b737c34d370007e
RBDSVIANA <rainier.viana@iesb.edu.br>
2024-08-27 21:33:22 -0300

    Adicionando arquivos da pratica04

---
+++ aulas/aula03/src/App.jsx
@@ -0,0 +1,11 @@
+import {Titulo} from './components/titulo'
+
+function App(){
+  return(
+    <div>
+      (console.log("Ola"))
+    </div>
+  )
+}
+
+export default App
---
+++ aulas/aula03/src/assets/components/titulo.jsx
@@ -0,0 +1,5 @@
+function Titulo(){
+    return <h1>Ola</h1>
+  }
+  
+  export {Titulo}
